5f1fffa1a0db82ae17671334c97047d47cb1cfee
Ewan Croft <contact@ewancroft.uk>
2026-07-31 03:35:18 +0100

    chore: bump to 0.2.6

--- include/wolfram/version.h
+++ include/wolfram/version.h
@@ -4,7 +4,7 @@
 /* Keep in sync with the project() version in CMakeLists.txt. */
 #define WOLFRAM_VERSION_MAJOR 0
 #define WOLFRAM_VERSION_MINOR 2
-#define WOLFRAM_VERSION_PATCH 5
-#define WOLFRAM_VERSION_STRING "0.2.5"
+#define WOLFRAM_VERSION_PATCH 6
+#define WOLFRAM_VERSION_STRING "0.2.6"
 
 #endif /* WOLFRAM_VERSION_H */
