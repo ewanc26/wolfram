3710a22d135116fc4c4ec4a7e567ac5ac7e10116
Ewan Croft <contact@ewancroft.uk>
2026-08-01 13:55:08 +0100

    version: bump to 0.6.1

--- include/wolfram/version.h
+++ include/wolfram/version.h
@@ -4,7 +4,7 @@
 /* Keep in sync with the project() version in CMakeLists.txt. */
 #define WOLFRAM_VERSION_MAJOR 0
 #define WOLFRAM_VERSION_MINOR 13
-#define WOLFRAM_VERSION_PATCH 0
-#define WOLFRAM_VERSION_STRING "0.13.0"
+#define WOLFRAM_VERSION_PATCH 1
+#define WOLFRAM_VERSION_STRING "0.13.1"
 
 #endif /* WOLFRAM_VERSION_H */
